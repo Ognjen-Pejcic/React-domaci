8fa95093fda2a675152c5913f467bc34259cd58d
Ognjen Pejčić <pejcicognjen@gmail.com>
2021-05-10 15:22:25 +0200

    Initial commit

---
+++ src/opis.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+import Header from './components/ui/header';
+const opis = () => {
+    return (
+       
+          <div></div>
+       
+    )
+}
+
+export default opis
