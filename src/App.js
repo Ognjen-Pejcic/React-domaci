ddb88ffaefdc2ff0380d410ca99f1be5fc8943ec
Ognjen Pejčić <pejcicognjen@gmail.com>
2021-05-10 16:16:14 +0200

    Konacan

--- src/App.js
+++ src/App.js
@@ -1,26 +1,30 @@
-import React, {useState, useEffect} from 'react'
+import React, { useState, useEffect } from 'react'
 import './App.css';
 import Header from './components/ui/header';
 import Search from './components/ui/Search';
 import CharacterGrid from './components/characters/CharacterGrid';
 import axios from 'axios';
-
-
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,Link
+} from "react-router-dom";
+import Opis from './opis'
 
 const App = () => {
   //items je to sto povlacimo sa api-a a setitems je funkcija koja nesto radi
   const [items, setItems] = useState([])
   const [isLoading, setIsLoading] = useState(true)
   const [query, setQuery] = useState(true)
-  
-  useEffect(async() => {
+
+  useEffect(async () => {
     const fetchItems = async () => {
       const result = await axios(`https://www.breakingbadapi.com/api/characters?name=${query}`)
-    
+
 
       console.log(result.data);
 
-     
+
 
       setItems(result.data)
       setIsLoading(false)
@@ -30,12 +34,64 @@ const App = () => {
 
 
   return (
-    <div className="container">
-      <Header />
-      <Search getQuery={(q) => setQuery(q)}/>
-      <CharacterGrid isLoading={isLoading} items={items}/>
-    </div>
+    <Router>
+      <div className="container">
+        <Header />
+
+
+        <Router>
+      <div>
+        <ul>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/opis">About</Link>
+          </li>
+        </ul>
+
+        <Switch>
+          <Route path="/opis">
+            <Opis />
+          </Route>
+       
+          <Route path="/">
+          <Search getQuery={(q) => setQuery(q)} />
+        <CharacterGrid isLoading={isLoading} items={items} />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
+
+        
+      </div>
+
+    </Router>
+
   );
 }
 
 export default App;
+// render(){
+//   return (
+//     <Router>
+//         <div className="App" style={this.state}>
+//           <header className="App-header">
+//             <div className="App-content">
+//               <Menu />
+//               <Switch>
+//                 <Route path="/details">
+//                   <Details state={this.state}/>
+//                 </Route>
+//                 <Route path="/dashboard">
+//                   <Content changeBackground={this.changeBackground} changeOfficialTitle={this.changeOfficialTitle}/>
+//                 </Route>
+//                 <Route path="/">
+//                   <Content changeBackground={this.changeBackground} changeOfficialTitle={this.changeOfficialTitle}/>
+//                 </Route>
+//               </Switch>
+//             </div>
+//           </header>
+//         </div>
+//     </Router>
+//   );
--- src/opis.js
+++ src/opis.js
@@ -1,12 +1,19 @@
 import React from 'react'
 
 import Header from './components/ui/header';
-const opis = () => {
+const Opis = () => {
     return (
        
-          <div></div>
+          <div>
+              <br></br>
+              <br></br>
+              Breaking Bad is an American neo-Western crime drama television series created and produced by Vince Gilligan. The show aired on AMC from January 20, 2008, to September 29, 2013, consisting of five seasons for a total of 62 episodes. It was set and filmed in Albuquerque, New Mexico, and tells the story of Walter White (Bryan Cranston), an underpaid, overqualified and dispirited high school chemistry teacher who is struggling with a recent diagnosis of stage-three lung cancer. White turns to a life of crime, partnering with his former student Jesse Pinkman (Aaron Paul), by producing and distributing crystal meth to secure his family's financial future before he dies, while navigating the dangers of the criminal underworld. According to Gilligan, the title is a Southern colloquialism meaning "to raise hell".
+          Among the show's co-stars are Anna Gunn and RJ Mitte as Walter's wife Skyler and son Walter, Jr., and Betsy Brandt and Dean Norris as Skyler's sister Marie Schrader and her husband Hank, a DEA agent. Others include Bob Odenkirk as White's and Pinkman's sleazy lawyer Saul Goodman, Jonathan Banks as private investigator and fixer Mike Ehrmantraut, and Giancarlo Esposito as drug kingpin Gus Fring. The final season introduces Jesse Plemons as the criminally ambitious Todd Alquist, and Laura Fraser as Lydia Rodarte-Quayle, a cunning business executive secretly managing Walter's global meth sales for her company.
+          Breaking Bad's first season received generally positive reviews, while the rest of its run received universal acclaim, which would make critics laud it as one of the greatest television series of all time. It had moderate viewership in its first three seasons, but the fourth and fifth seasons saw a significant rise in viewership when it was made available on Netflix just before the fourth season premiere. By the time that the series finale aired, it was among the most-watched cable shows on American television. The show received numerous awards, including 16 Primetime Emmy Awards, eight Satellite Awards, two Golden Globe Awards, two Peabody Awards, two Critics' Choice Awards, and four Television Critics Association Awards. Cranston won the Primetime Emmy Award for Outstanding Lead Actor in a Drama Series four times, while Aaron Paul won the Primetime Emmy Award for Outstanding Supporting Actor in a Drama Series three times; Anna Gunn won the Primetime Emmy Award for Outstanding Supporting Actress in a Drama Series twice. In 2013, Breaking Bad entered the Guinness World Records as the most critically acclaimed show of all time.
+          Better Call Saul, a prequel series featuring Odenkirk, Banks, and Esposito reprising their Breaking Bad roles, debuted on AMC on February 8, 2015, and has been renewed for a final sixth season. The sequel film El Camino: A Breaking Bad Movie starring Aaron Paul was released on Netflix and in theaters on October 11, 2019.
+          </div>
        
     )
 }
 
-export default opis
+export default Opis
